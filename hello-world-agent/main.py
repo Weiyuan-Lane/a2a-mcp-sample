8b653b8d9c058d5fdc138eb1a67510076f5917e5
Weiyuan Liu <weiyuan.lane@gmail.com>
2025-06-29 15:09:15 +0800

    [Agent] Added new agent with integrating A2A here

--- hello-world-agent/main.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-from src.server import agent_application
-
-if __name__ == "__main__":
-  config = uvicorn.Config(agent_application.build(), host="0.0.0.0", port=8080, log_level="info", reload_dirs=["/app/src"])
-  server = uvicorn.Server(config)
-  server.run()
